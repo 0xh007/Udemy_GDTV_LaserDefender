e6a642d9f141db201a91af1575cc8ff57edd980d
Victor <victor.r.rogers@gmail.com>
2019-06-11 20:03:02 -0600

    added waypoints for enemy pathing and also a collider to destroy lasers going offscreen

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+        #region Configuration Parameters
+
+        [SerializeField]
+        private List<Transform> waypoints;
+
+        #endregion
+
+        #region State
+
+        #endregion
+
+        #region Cached References
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+}
