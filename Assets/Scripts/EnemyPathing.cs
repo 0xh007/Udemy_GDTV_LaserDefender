8c88db4adf3599983c3845f2566f336260cc4c1e
Victor <victor.r.rogers@gmail.com>
2019-06-27 07:44:52 -0600

    Not sure what's in this change list

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -9,13 +9,51 @@ public class EnemyPathing : MonoBehaviour
         [SerializeField]
         private List<Transform> waypoints;
 
+        [SerializeField]
+        private float moveSpeed = 2f;
+
         #endregion
 
         #region State
+        
+        private int waypointIndex = 0;
 
         #endregion
 
         #region Cached References
+
+        #endregion
+
+        #region Event Methods 
+
+        private void Start()
+        {
+            transform.position = waypoints[waypointIndex].transform.position;
+        }
+
+        private void Update()
+        {
+            MoveToWaypoint();
+        }
+
+        private void MoveToWaypoint()
+        {
+            if (waypointIndex <= waypoints.Count - 1)
+            {
+                var targetPosition = waypoints[waypointIndex].transform.position;
+                var movementThisFrame = moveSpeed * Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+                if (transform.position == targetPosition)
+                {
+                    waypointIndex++;
+                }
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
 
         #endregion
 
